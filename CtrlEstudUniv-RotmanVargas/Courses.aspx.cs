649b70e12299e1fbc2da5ac2fa86d605b33b4272
Rotman Vargas <rotman.vargas@gmail.com>
2022-10-19 23:54:08 -0600

    se agregan cursos y carreras

--- CtrlEstudUniv-RotmanVargas/Courses.aspx.cs
+++ CtrlEstudUniv-RotmanVargas/Courses.aspx.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using Antlr.Runtime.Misc;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.Configuration;
 
 public partial class Courses : System.Web.UI.Page
 {
@@ -11,4 +18,11 @@ public partial class Courses : System.Web.UI.Page
     {
 
     }
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
 }
+       
